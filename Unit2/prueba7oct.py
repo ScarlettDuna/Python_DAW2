b19830eaa618e329dfb7807b4aa9ac0731b816a5
ScarlettDuna <arantxa.rg.1991@hotmail.com>
2025-10-08 12:32:19 +0200

    mejoras a doc prueba7oct.py

--- Unit2/prueba7oct.py
+++ Unit2/prueba7oct.py
@@ -1,11 +1,17 @@
 # 1. qué día de la semana se produce el mayor número de ventas
 # 2. qué día de la semana se produce el menor número de ventas
 # 3. si las ventas del domingo superan a la media semanal.
+def DomingoSemana(domingo, media):
+    if domingo > media:
+        print("Sí")
+    else:
+        print("No")
+
 dias = ["MARTES", "MIÉRCOLES", "JUEVES", "VIERNES", "SÁBADO", "DOMINGO"]
-recaudacion = [None, None, None, None, None, None]
+recaudacion = []
 try:
     for i in range(6):
-        recaudacion[i] = int(input(f"Inserte la recaudación del {dias[i]} "))
+        recaudacion.append(int(input(f"Inserte la recaudación del {dias[i]} ")))
 except ValueError:
     print("Debes introducir números")
 
@@ -20,12 +26,8 @@ for i in range(6):
         menosVentas = i
     total += recaudacion[i]
     print(f"Recaudación del {dias[i]}: {recaudacion[i]}")
-mediaSemana = total / 6
+mediaSemana = total / len(recaudacion)
 print("Mejor día: ", dias[masVentas])
 print("Peor día: ", dias[menosVentas])
-def DomingoSemana(domingo, media):
-    if domingo > media:
-        print("Sí")
-    else:
-        print("No")
+
 DomingoSemana(recaudacion[5], mediaSemana)
